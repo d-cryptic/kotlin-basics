7f62a27a213ff129f8a69050315c01fa627b4e79
Barun Debnath <barundebnath91@gmail.com>
2021-10-24 01:31:41 +0530

    removed references

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,17 +16,19 @@ class MainActivity : AppCompatActivity() {
 
 
         // get reference to button
-        val btn_click_me = findViewById(R.id.button) as Button
+//        val btnClickMe = findViewById(R.id.button) as Button
+
+
 
         // reference to the text
-        val myTextView = findViewById(R.id.textView) as TextView
+//        val myTextView = findViewById(R.id.textView) as TextView
         var timesClicked = 0
 
         // set on-click listener
-        btn_click_me.setOnClickListener {
+        button.setOnClickListener {
 //            myTextView.text = "Text"
             timesClicked += 1
-            myTextView.text = timesClicked.toString()
+            textView.text = timesClicked.toString()
 
             Toast.makeText(this@MainActivity, "Hi Denis", Toast.LENGTH_SHORT).show()
 
