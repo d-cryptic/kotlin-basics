d55ffb5b18ed5e832ee97654e9a2213adc5fd7e0
Barun Debnath <barundebnath91@gmail.com>
2021-10-26 01:38:20 +0530

    getter setter added

---
+++ app/src/main/java/com/example/helloworld/KotlinSetGet.kt
@@ -0,0 +1,43 @@
+package com.example.helloworld
+
+import java.lang.IllegalArgumentException
+
+fun main(){
+    var myCar = Car()
+//    myCar.owner
+    println("brand is : ${myCar.myBrand}")
+    myCar.maxSpeed = 200
+    println("Maxspeed is ${myCar.maxSpeed }")
+//    myCar.model = "M3"
+    println("Model is ${myCar.myModel}")
+}
+
+class Car(){
+    // Initialize the variable later on
+    lateinit var owner: String
+
+    val myBrand: String = "BMW"
+
+//        Custom getter
+    get(){
+        return field.toLowerCase()
+    }
+
+    var maxSpeed: Int = 250
+        get() = field
+        set(value){
+            field = if(value > 0) value else throw IllegalArgumentException("Maximum speed cannont be less than 0")
+        }
+
+    var myModel : String = 'MS'
+        private set
+
+
+    init{
+        this.myModel = "M3"
+        this.owner = "Frank"
+    }
+}
+
+//field - vacant field helps you to get the values of getter
+// value - value given to the method when we use setter
