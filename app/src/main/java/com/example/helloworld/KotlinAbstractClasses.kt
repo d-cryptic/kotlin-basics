8b0911ac34cb692fc0437bb0ca24d7c23eec6ac1
Barun Debnath <barundebnath91@gmail.com>
2021-10-26 15:09:57 +0530

    Abstract class done

---
+++ app/src/main/java/com/example/helloworld/KotlinAbstractClasses.kt
@@ -0,0 +1,61 @@
+package com.example.helloworld
+
+//Abstract class cannot be instantiated
+// you cannot create objects of an abstract class
+// However you can inherit subclasses from an abstract class
+//The members (properties and methods) of an abstract class are non-abstract
+//unless you explicitly use the abstract keyword to make them abstract
+
+abstract class Mammal(private val name: String, private val origin: String, private val weight: Double){
+//    Concrete (non abstract) properties
+
+//    Abstract property (must be overridden by subclasses
+    abstract var maxSpeed: Double
+
+//    Abstract methods (must be implemented by subclasses)
+    abstract fun run()
+    abstract fun breath()
+
+//    Concrete (Non Abstract) Method
+    fun displayDetails(){
+        println("Name: $name, Origin: $origin, Weight: $weight, " + "Max Speed: $maxSpeed")
+    }
+}
+
+class Human(name: String, origin: String, weight: Double, override var maxSpeed: Double): Mammal(name, origin, weight){
+    override  fun run(){
+//        code to run
+        println("Runs on two legs")
+    }
+
+    override fun breath(){
+//        code to breath
+        println("Breath thorugh mouth or nose")
+    }
+}
+
+class Elephant(name:String, origin: String, weight: Double, override var maxSpeed: Double): Mammal(name, origin, weight){
+    override fun run(){
+//        Code to run
+        println("Runs on four legs")
+    }
+
+    override fun breath(){
+//        code to breath
+        println("Breath through the trunk")
+    }
+}
+
+fun main(){
+    val human = Human("Denis", "Russia", 70.0, 28.0)
+    val elephant = Elephant("Rosy", "India", 5400.0, 25.0)
+
+//    val mammal = Mammal("denis", "Russia", 70.0, 28.0) - cant do this
+
+
+
+    human.run()
+    elephant.run()
+    human.breath()
+    elephant.breath()
+}
